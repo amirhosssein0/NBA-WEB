578739288ce617034d5692096f51e54f14074af6
amirhosein <real.amirhosssein0@gmail.com>
2025-06-03 18:55:04 +0330

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
-from accounts.models import User,Profile
+from accounts.models import User
 
 class SignUpForm(UserCreationForm):
     class Meta(UserCreationForm):
